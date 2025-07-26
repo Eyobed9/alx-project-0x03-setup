4cb346602f9101ba6624392fd97c1a6f1ef2abee
Eyobed <eyobedteshome@gmail.com>
2025-07-26 22:56:58 +0300

    landing page with Imperative routing has been set up :

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -23,7 +23,7 @@ const Button = ({
 	return (
 		<button
 			onClick={action}
-			className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}
+			className={`${backgroundColorClass} ${buttonSize} px-6 py-2 cursor-pointer text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}
 		>
 			{buttonLabel}
 		</button>
